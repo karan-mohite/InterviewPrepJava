9c993fe5ce2cb7c901ede35dd97d394a32c51058
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-03 18:02:45 +0530

    .

---
+++ Amit.java
@@ -0,0 +1,16 @@
+package Package1;
+public class Amit
+{	
+	private int x;
+	private int y;
+	public void setData(int p,int q)
+	{		
+		x=p;
+		y=q;
+	}
+	public void display()
+	{	
+		System.out.println("Value of X is :"+x);
+		System.out.println("Value of Y is :"+y);
+	}
+}
