609830a9d8d554969040e5d66e30611017e3aee9
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-03 23:20:22 +0530

    .

--- ThisDemo.java
+++ ThisDemo.java
@@ -1,8 +1,8 @@
 //This keyword Demo
 class Example
 {	
-	private int x;
-	private int y;
+	  int x;
+	  int y;
 	
 	void f1(int x,int y)
 	{
@@ -10,19 +10,41 @@ class Example
 		this.y=y;
 	}
 	void display()
-	{	
-		System.out.println(x);
-		System.out.println(y);		
+	{		
+		System.out.println(this.x);
+		System.out.println(this.y);		
 	}
 }
-
+class Example1 extends Example
+{	
+	int x,y;
+	void f3(int x,int y)
+	{	
+		super.x=x;
+		super.y=y;
+	}
+	void f4()
+	{	
+		
+		System.out.println(super.x);
+		System.out.println(this.y);		
+	}
+	
+}
 class ThisDemo
 {	
 	public static void main(String args[])
 	{	
+		Example1 e1=new Example1();
+		e1.f3(7,9);
+		e1.f4();
+		
+		/*
 		Example e1=new Example();
 		e1.f1(10,20);
 		e1.display();
 		Example e2=new Example();
+		
+		*/
 	}
 }
