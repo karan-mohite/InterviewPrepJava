f6aea48e4754d84bdabdb7b1a7c290f6a17a66ad
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-03 22:59:21 +0530

    .

---
+++ ThisDemo.java
@@ -0,0 +1,26 @@
+class Example
+{	
+	private int x;
+	private int y;
+	
+	void f1(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	void display()
+	{	
+		System.out.println(x);
+		System.out.println(y);		
+	}
+}
+
+class ThisDemo
+{	
+	public static void main(String args[])
+	{	
+		Example e1=new Example();
+		e1.f1(10,20);
+		e1.display();
+	}
+}
