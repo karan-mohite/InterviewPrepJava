4c58c047b3cd32eef6cecd3c8a8390298cab2f6a
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-03 12:09:08 +0530

    .

--- StaticMembers.java
+++ StaticMembers.java
@@ -1,14 +1,15 @@
 class A
 {	
-	int x;
+	private int x;
 	private static int y;
 	void fun1()
 	{	
 		y=22;
+		x=5;
 	}
 	void fun2()
 	{	
-		System.out.println(y);
+		System.out.println(x);
 	}
 	void fun3()
 	{	
@@ -19,10 +20,9 @@ class StaticMembers
 {	
 	public static void main(String args[])
 	{	
-		A a1=new A();
+		A a1=new A();	
 		A a2=new A();
 		a1.fun1();
-		a2.fun3();
-		a1.fun2();
-	}
+		a2.fun2();
+ 	}
 }
