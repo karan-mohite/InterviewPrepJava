3b3c86348ca2677d467d5a4bbbe3fcde4d0a1137
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-03 16:22:17 +0530

    .

--- Object2.java
+++ Object2.java
@@ -1,10 +1,12 @@
 class Mahesh
 {	
-	void fun1()
+	int x;	//instance(object) member  variable
+	private int y;	//instance(object) member  variable
+	void fun1()	//instance(object) member function
 	{	
 		System.out.println("Fun1");
 	}
-	void fun2()
+	void fun2() //instance(object) member function
 	{			
 	   System.out.println("Fun2");
 	}
@@ -14,7 +16,10 @@ class Object2
 	public static void main(String args[])
 	{	
 		Mahesh m1=new Mahesh();
-		m1.fun1();
-		m1.fun2();
+		Mahesh m2=new Mahesh();
+		m1.x=5;
+		m2.x=51;
+ 		System.out.println(m2.x);
+ 		//System.out.println(m2.y);
 	}	
 }
