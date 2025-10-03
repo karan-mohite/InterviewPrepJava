3a44c23564f8cd9ba5850fad5f9f3aeecf88727b
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-03 17:17:01 +0530

    .

--- CLA.java
+++ CLA.java
@@ -1,10 +1,10 @@
 class CLA
 {	
 	public static void main(String args[])
-	{	    
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-		System.out.println(args[2]);
-		System.out.println(args[3]);	
+	{	
+		for(int i=0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+		}		
 	}		
 }
