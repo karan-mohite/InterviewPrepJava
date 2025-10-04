94fa314f6799bc9d51676022222efa1c838c31cb
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-04 21:54:48 +0530

    .

--- LoginSwingDemo.java
+++ LoginSwingDemo.java
@@ -4,7 +4,7 @@ class LoginSwingDemo extends JFrame
 {	
 	JLabel l1,l2,l3,l4;
 	JTextField t1;
-	JButton b1,b2;
+	JButton b1,b2,b3;
 	JPasswordField t2;
 	
 	LoginSwingDemo(String s1)
@@ -25,6 +25,7 @@ class LoginSwingDemo extends JFrame
 		t2=new JPasswordField();
 		b1=new JButton("Login");
 		b2=new JButton("Clear");
+		b3=new JButton("ADD");
 		setLayout(null);
 		
 		add(l1);
@@ -35,18 +36,21 @@ class LoginSwingDemo extends JFrame
 		add(t2);
 		add(b1);
 		add(b2);
+		add(b3);
+		
 		l1.setBounds(100,50,300,30);
 		l2.setBounds(100,200,100,30);
 		l3.setBounds(100,350,100,30);
-		l4.setBounds(100,550,200,30);
+		l4.setBounds(100,550,400,30);
 		t1.setBounds(350,200,100,30);
 		t2.setBounds(350,350,100,30);
 		b1.setBounds(200,450,100,30);
 		b2.setBounds(400,450,100,30);
+		b3.setBounds(500,550,100,30);
 		
 		b1.addActionListener(new Log());
 		b2.addActionListener(new Clear());
-		
+		b3.addActionListener(new Add());
 		
 	}
 	public static void main(String args[])
@@ -79,6 +83,23 @@ class LoginSwingDemo extends JFrame
 		{	
 			t1.setText("");
 			t2.setText("");
+		}	
+	}
+	class Add implements ActionListener
+	{	
+		public void actionPerformed(ActionEvent e1) 
+		{	
+		    try
+			{
+			 int a=Integer.parseInt(t1.getText());
+			 int b=Integer.parseInt(t2.getText());			 
+			 int c=a+b;
+			 l4.setText("Addition is :"+c);
+			}
+			catch(Exception e)
+			{
+				l4.setText("Please Enter a number only..!!");
+			}
 		}	
 	}
 }
