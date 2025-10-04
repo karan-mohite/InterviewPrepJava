f4e16e37742fc3768f821bec38b6c3c92f13b31f
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-04 18:29:15 +0530

    .

---
+++ Login.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+class LoginSwingDemo
+{	
+	public static void main(String args[])
+	{	 
+		
+	}
+}
---
+++ LoginSwingDemo.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class LoginSwingDemo
+{	
+	public static void main(String args[])
+	{	 
+		JFrame j1=new JFrame();
+		j1.setVisible(true);
+		j1.setSize(500,500);
+		j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
