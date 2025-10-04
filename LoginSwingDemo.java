4f919d3cd528878dfe7d68df129fd37a38f623f6
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-04 18:47:42 +0530

    .

--- LoginSwingDemo.java
+++ LoginSwingDemo.java
@@ -5,8 +5,26 @@ class LoginSwingDemo extends JFrame
 	{	
 		super(s1);
 	}
+	LoginSwingDemo()
+	{	
+		
+	}
+	void setComponents()
+	{	
+		JLabel l1=new JLabel("WELCOME");
+		JTextField t1=new JTextField();
+		setLayout(null);
+		l1.setBounds(200,100,100,30);
+		t1.setBounds(200,100,100,30);
+		add(t1);
+		add(l1);
+	}
 	public static void main(String args[])
 	{
 		LoginSwingDemo s1=new LoginSwingDemo("Welcome to FaceBook..!!");
+		s1.setVisible(true);
+		s1.setSize(700,700);
+		s1.setComponents();
+		s1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
