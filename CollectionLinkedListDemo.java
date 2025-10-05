8271e3050426bc4ff3617b7ed645d4450aed92b7
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-05 13:07:34 +0530

    .

---
+++ CollectionLinkedListDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class CollectionLinkedListDemo
+{	
+	public static void main(String args[])
+	{	
+		LinkedList l1=new LinkedList();
+		
+		l1.addFirst("Coding");
+		l1.addLast("Seekho");
+		l1.add(1,"College");
+		
+		System.out.println(l1.get(1));
+		System.out.println(l1.getFirst());
+		System.out.println(l1.getLast());		
+	}			
+}
