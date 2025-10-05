5407e4e69aa49c2f66eaa9d6d8a0b63eeb0a9610
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-05 18:02:25 +0530

    .

---
+++ HashSetDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HashSetDemo
+{	
+	public static void main(String args[])
+	{	
+		HashSet h1=new HashSet();
+		System.out.println(h1.add("C"));
+		System.out.println(h1.add("C++"));
+		System.out.println(h1.add("Core Java"));
+ 		System.out.println(h1.add("Python"));
+		System.out.println(h1);
+	}
+}
