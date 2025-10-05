e5e71040073386e3ac537b48443ee86942c8e602
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-05 11:54:23 +0530

    .

---
+++ CollectionArrayListDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class CollectionArrayListDemo
+{	
+	public static void main(String args[])
+	{       
+		ArrayList a1=new ArrayList(5);
+		a1.add("A");
+		a1.add("B");
+		a1.add("C");
+		System.out.println(a1);
+	}
+}
+
+	
